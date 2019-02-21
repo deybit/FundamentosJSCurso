219d81627818857353b75ddfac97da365c3f8ff1
deybit <deybitgutierrez@gmail.com>
2019-02-21 08:49:34 -0400

    String functions

--- clase1.js
+++ clase1.js
@@ -12,6 +12,23 @@ var peso = 63
 var nombreMayusculas = nombre.toUpperCase()
 var apellidoMinusculas = apellido.toLowerCase()
 
+console.log(nombreMayusculas.charAt(0))
+console.log('Cantidad de letras de apellido ' + apellido.length)
+
+var nombreCompleto = nombre +' '+ apellido
+
+var nombreInterpolado = `${nombre} ${apellido}`
+
+var caracterNombre = nombre.charAt(1) + nombre.charAt(2)
+var caracterNombre2 = nombre.substr(1 , 4)
+
+/* RETO: Pedir al usuario nombre y mostrar la ultima letra */
+
+
+var nLetras = nombre.length
+
+console.log('La ultima letra de tu nombre es: ' + nombre.charAt(nLetras -1))
+
 /* var nombre = 'Sacha', apellido = 'Lifszyc'
 var edad = 28
 
