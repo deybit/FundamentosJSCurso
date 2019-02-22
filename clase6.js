e87ef6ee6ecf8358b6652f67a9e634470aa94c10
deybit <deybitgutierrez@gmail.com>
2019-02-22 17:23:26 -0400

    End of function lesson

--- clase6.js
+++ clase6.js
@@ -1,4 +1,39 @@
-var sacha = {
+var maria = {
+  nombre: 'Maria',
+  apellido: 'Mendez',
+  edad: 23
+
+}
+
+var jose = {
+  nombre: 'Jose',
+  apellido: 'Martinez',
+  edad: 24
+
+}
+
+//var nombre2 = 'Damian'
+
+function imprimirNombreEnMayusculas(persona) {
+  nombre = persona.nombre.toUpperCase()
+  console.log(nombre)
+}
+
+function imprimirNombreEnMayusculas2({ nombre }) {
+  nombre2 = nombre.toUpperCase()
+  console.log(nombre2)
+}
+
+imprimirNombreEnMayusculas(maria)
+imprimirNombreEnMayusculas(jose)
+imprimirNombreEnMayusculas2({nombre: 'Alex'})
+
+
+
+
+
+
+/*var sacha = {
   nombre: 'Sacha',
   apellido: 'Lifszyc',
   edad: 28
@@ -18,3 +53,4 @@ imprimirNombreEnMayusculas(sacha)
 imprimirNombreEnMayusculas(dario)
 // imprimirNombreEnMayusculas({ nombre: 'Pepito' })
 imprimirNombreEnMayusculas({ apellido: 'Gomez' })
+*/
