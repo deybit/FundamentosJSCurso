81d847691bff3393ae35bd10c06df77ae6b10fec
deybit <deybitgutierrez@gmail.com>
2019-02-23 18:06:34 -0400

    While

--- clase14.js
+++ clase14.js
@@ -1,4 +1,36 @@
-var sacha = {
+var jose = {
+  nombre: 'Jose',
+  apellido: 'Aleman',
+  edad: 24,
+  peso: 62
+}
+
+console.log(`Al principio de año ${jose.nombre} pesa ${jose.peso} kilogramos`)
+
+const aumentarDePeso = (persona, incremento) => {
+  persona.peso += incremento
+}
+
+const bajarDePeso = (persona, decremento) => {
+  persona.peso -= decremento
+}
+var i = 0
+var meta = jose.peso -3
+while (jose.peso > meta){
+  //debugger
+  i +=1
+  var random = Math.random()
+  if (random < 0.25){
+    aumentarDePeso(jose,Math.random())
+  }else if ( random < 0.50){
+    bajarDePeso(jose,Math.random())
+  }
+}
+
+console.log(`${jose.nombre} logro la meta y llego al peso de ${jose.peso.toFixed(2)} kilogramos en ${i} dias`)
+
+
+/* var sacha = {
   nombre: 'Sacha',
   apellido: 'Lifszyc',
   edad: 28,
@@ -32,3 +64,4 @@ while (sacha.peso > META) {
 }
 
 console.log(`Pasaron ${dias} días hasta que ${sacha.nombre} adelgazó 3kg`)
+*/
