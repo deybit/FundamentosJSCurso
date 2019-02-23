45f38b8f3a9d4f1cdf61751b5d8cac5168a2ae4c
deybit <deybitgutierrez@gmail.com>
2019-02-23 15:17:44 -0400

    Comparisons

--- clase9.js
+++ clase9.js
@@ -1,5 +1,25 @@
 var x = 4, y = '4'
 
+var maria = {
+  nombre: 'Maria',
+  apellido: 'Mendez',
+  edad: 23
+
+}
+
+var jose = {
+  nombre: 'Maria',
+  apellido: 'Mendez',
+  edad: 23
+
+}
+
+var maria2 = maria
+
+
+
+/* var x = 4, y = '4'
+
 var sacha = {
   nombre: 'Sacha'
 }
@@ -7,3 +27,4 @@ var sacha = {
 var otraPersona = {
   nombre: 'Sacha'
 }
+*/
