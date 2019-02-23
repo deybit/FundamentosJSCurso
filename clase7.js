ad8468383f9d3900cc856532851e9878ae9d2194
deybit <deybitgutierrez@gmail.com>
2019-02-23 14:17:08 -0400

    End of 7th lesson

--- clase7.js
+++ clase7.js
@@ -1,4 +1,49 @@
-var sacha = {
+var maria = {
+  nombre: 'Maria',
+  apellido: 'Mendez',
+  edad: 23
+
+}
+
+var jose = {
+  nombre: 'Jose',
+  apellido: 'Martinez',
+  edad: 24
+
+}
+
+//var nombre2 = 'Damian'
+
+function imprimirNombreEnMayusculas(persona) {
+
+  var { nombre } = persona
+  //nombre = persona.nombre.toUpperCase()
+  console.log(nombre)
+}
+
+function imprimirNombreYEdad (persona){
+
+  var { nombre } = persona
+  var { edad } = persona
+  console.log('Hola me llamo ' + nombre + ' y tengo ' + edad + ' años');
+  //hola me llamo tal y tengo x años
+}
+
+
+function imprimirNombreEnMayusculas2({ nombre }) {
+  nombre2 = nombre.toUpperCase()
+  console.log(nombre2)
+}
+
+imprimirNombreEnMayusculas(maria)
+imprimirNombreEnMayusculas(jose)
+imprimirNombreYEdad(jose)
+imprimirNombreEnMayusculas2({nombre: 'Alex'})
+
+
+
+
+/*var sacha = {
   nombre: 'Sacha',
   apellido: 'Lifszyc',
   edad: 28
@@ -25,3 +70,4 @@ function imprimirNombreYEdad() {
   // Hola, me llamo Sacha y tengo 28 años
   // Hola, me llamo Darío y tengo 27 años
 }
+*/
