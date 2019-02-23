5db509276845bcc7d33aa0b3ce7c23b1ee5a884d
deybit <deybitgutierrez@gmail.com>
2019-02-23 16:58:14 -0400

    Arrow function

--- clase12.js
+++ clase12.js
@@ -1,4 +1,53 @@
-var sacha = {
+var jose = {
+  nombre: 'Jose',
+  apellido: 'Aleman',
+  edad: 24,
+  ingeniero: true,
+  dj: false,
+  guitarrista: false,
+  piloto:true
+}
+
+function imprimirSiEsMayorDeEdad(persona){
+  if(mayorDeEdad(persona)>17){
+    //console.log(persona.nombre + ' es mayor de edad')
+    console.log(`${persona.nombre} es mayor de edad`)
+  }else{
+    // console.log(persona.nombre + ' es menor de edad')
+    console.log(`${persona.nombre} es menor de edad`)
+  }
+}
+
+var mayorDeEdad4 = function (persona){
+  if (persona.edad >17){
+    return true
+  }
+}
+
+var mayorDeEdad2 = (persona) => {
+  if (persona.edad >17){
+    return true
+  }
+}
+
+//const mayorDeEdad3 = persona => persona.edad >= 18
+
+const mayorDeEdad = ({ edad }) => edad >= 18
+const menorDeEdad = ({ edad }) => !mayorDeEdad({ edad })
+
+function verificarAcceso(persona){
+  if (menorDeEdad(persona)){
+    console.log('Acceso denegado')
+  }else{
+    console.log('Acceso permitido')
+  }
+}
+
+
+/*Tdodos los codigos de mayorDeEdad son equivalentes */
+
+
+/* var sacha = {
   nombre: 'Sacha',
   apellido: 'Lifszyc',
   edad: 28,
@@ -66,3 +115,4 @@ function permitirAcceso(persona) {
     console.log('ACCESO DENEGADO')
   }
 }
+*/
