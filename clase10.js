59a58ef9cdba53d1091e82d9b118ee1096e10816
deybit <deybitgutierrez@gmail.com>
2019-02-23 15:40:52 -0400

    If

--- clase10.js
+++ clase10.js
@@ -1,4 +1,37 @@
-var sacha = {
+var maria = {
+  nombre: 'Maria',
+  apellido: 'Mendez',
+  edad: 23
+
+}
+
+var jose = {
+  nombre: 'Jose',
+  apellido: 'Aleman',
+  edad: 23,
+  ingeniero: true,
+  dj: false,
+  guitarrista: false,
+  piloto:true
+}
+
+function imprimirProfesiones(persona) {
+  console.log(`${persona.nombre} es:`)
+
+  if(persona.ingeniero){
+    console.log('Ingeniero')
+  }
+}
+
+function imprimirSiEsMayorDeEdad(persona){
+  if(persona.edad>17){
+    console.log(persona.nombre + ' es mayor de edad')
+  }else{
+    console.log('Es menor de edad')
+  }
+}
+
+/* var sacha = {
   nombre: 'Sacha',
   apellido: 'Lifszyc',
   edad: 28,
@@ -42,3 +75,4 @@ function imprimirProfesiones(persona) {
 }
 
 imprimirProfesiones(sacha)
+*/
