100e704b64c2377030b8b4e93ba9771784809712
deybit <deybitgutierrez@gmail.com>
2019-02-24 00:47:25 -0400

    Prototypes h

--- clase24.js
+++ clase24.js
@@ -1,4 +1,40 @@
-function heredaDe(prototipoHijo, prototipoPadre) {
+function heredaDe(protoHijo, protoPadre){
+  var fn = function () {} //Funcion dummy
+  fn.protoype = protoPadre.prototype
+  protoHijo.prototype = new fn //Creamos un objeto nuevo para no pisar a padre
+  protoHijo.prototype.constructor = protoHijo
+}
+
+function Persona(nombre, apellido, estatura) {
+  this.nombre = nombre
+  this.apellido = apellido
+  this.estatura = estatura
+}
+
+Persona.prototype.soyAlto = function(){
+  if(this.estatura >= 180) console.log(`Mi nombre es ${this.nombre} y soy alt@`)
+}
+
+Persona.prototype.saludar = function(){
+  console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`)
+}
+
+function Desarrollador(nombre, apellido){
+  this.nombre = nombre
+  this.apellido = apellido
+}
+
+var sacha = new Persona('Sacha', 'Diaz', 181)
+var jose = new Persona('Jose', 'Arellano', 151)
+var maria = new Persona('Maria', 'Hernandez', 191)
+sacha.saludar() //Saluda al momento de ser creada
+sacha.soyAlto()
+
+heredaDe(Desarrollador, Persona)
+
+
+
+/* function heredaDe(prototipoHijo, prototipoPadre) {
   var fn = function () {}
   fn.prototype = prototipoPadre.prototype
   prototipoHijo.prototype = new fn
@@ -30,7 +66,7 @@ Desarrollador.prototype.saludar = function () {
   console.log(`Hola, me llamo ${this.nombre} ${this.apellido} y soy desarrollador/a`)
 }
 
-
+*/
 // var sacha = new Persona('Sacha', 'Lifszyc', 1.72)
 // var erika = new Persona('Erika', 'Luna', 1.65)
 // var arturo = new Persona('Arturo', 'Martinez', 1.89)
