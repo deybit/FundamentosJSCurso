42efde0507e59e06e0a81922cca32873381b33aa
deybit <deybitgutierrez@gmail.com>
2019-02-23 23:33:12 -0400

    Prototypes

--- clase21.js
+++ clase21.js
@@ -1,4 +1,31 @@
-function Persona(nombre, apellido) {
+function Persona(nombre, apellido, estatura) {
+  //console.log('Ejecucion en proceso')
+  this.nombre = nombre
+  this.apellido = apellido
+  this.estatura = estatura
+  //return this
+}
+
+Persona.prototype.soyAlto = function(){
+  if(this.estatura >= 180) console.log(`Mi nombre es ${this.nombre} y soy alt@`)
+}
+
+Persona.prototype.saludar = function(){
+  console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`)
+}
+
+
+
+var sacha = new Persona('Sacha', 'Diaz', 181)
+sacha.saludar() //Saluda al momento de ser creada
+sacha.soyAlto()
+
+
+
+
+
+
+/* function Persona(nombre, apellido) {
   this.nombre = nombre
   this.apellido = apellido
 }
@@ -10,3 +37,4 @@ Persona.prototype.saludar = function () {
 var sacha = new Persona('Sacha', 'Lifszyc')
 var erika = new Persona('Erika', 'Luna')
 var arturo = new Persona('Arturo', 'Martinez')
+*/
