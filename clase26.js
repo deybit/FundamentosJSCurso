4950ca90600f7df691d8d754c79819a202d34924
deybit <deybitgutierrez@gmail.com>
2019-02-24 01:26:58 -0400

    Function as parameters

--- clase26.js
+++ clase26.js
@@ -1,4 +1,53 @@
 class Persona {
+  constructor(nombre, apellido, estatura){
+    this.nombre = nombre
+    this.apellido = apellido
+    this.estatura = estatura
+  }
+
+  saludar(fn){
+    console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`)
+    if(fn){
+      fn(this.nombre, this.apellido, false)
+    }
+  }
+
+  soyAlto(){
+      if(this.estatura >= 180) console.log(`Mi nombre es ${this.nombre} y soy alt@`)
+  }
+
+}
+
+class Desarrollador extends Persona {
+  constructor(nombre, apellido, estatura){
+    super(nombre, apellido, estatura)
+  }
+
+  saludar(fn){
+    console.log(`Hola, me llamo ${this.nombre} ${this.apellido} y soy desarrollador/a`)
+    if(fn){
+      fn(this.nombre, this.apellido, true)
+    }
+  }
+
+}
+
+function responderSaludo(nombre, apellido, esDev){
+  console.log(`Buen dia ${nombre} ${apellido}`)
+  if (esDev){
+    console.log(`Que bueno es saberte desarrollador/a`)
+  }
+}
+
+var sacha = new Persona('Sacha', 'Lifszyc', 1.72)
+var erika = new Persona('Erika', 'Luna', 1.65)
+var arturo = new Desarrollador('Arturo', 'Martinez', 1.89)
+
+sacha.saludar(responderSaludo)
+erika.saludar(responderSaludo)
+arturo.saludar(responderSaludo)
+
+/* class Persona {
   constructor(nombre, apellido, altura) {
     this.nombre = nombre
     this.apellido = apellido
@@ -50,3 +99,4 @@ var arturo = new Desarrollador('Arturo', 'Martinez', 1.89)
 sacha.saludar()
 erika.saludar(responderSaludo)
 arturo.saludar(responderSaludo)
+*/
