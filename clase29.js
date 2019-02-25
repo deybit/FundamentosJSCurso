92d7aaa438397a6c7d9fbb17691fc9e83e0dbfef
deybit <deybitgutierrez@gmail.com>
2019-02-25 17:13:20 -0400

    Multiple query

--- clase29.js
+++ clase29.js
@@ -1,5 +1,32 @@
 const API_URL = 'https://swapi.co/api/'
 const PEOPLE_URL = 'people/:id'
+
+//const lUrl = `${API_URL}${PEOPLE_URL.replace(':id','1')}`
+const opts = { crossDomain: true }
+
+const onResponse = function (persona){
+  console.log(`Hola yo soy ${persona.name}`)
+}
+
+function obtenerPersonaje(id){
+  const URL = `${API_URL}${PEOPLE_URL.replace(':id',id)}`
+  $.get(URL, opts, onResponse)
+}
+
+obtenerPersonaje(1)
+obtenerPersonaje(2)
+obtenerPersonaje(3)
+obtenerPersonaje(4)
+obtenerPersonaje(5)
+obtenerPersonaje(6)
+//$.get(lUrl, opts, onResponse)
+
+
+
+
+
+/* const API_URL = 'https://swapi.co/api/'
+const PEOPLE_URL = 'people/:id'
 const opts = { crossDomain: true }
 
 const onPeopleResponse = function (persona) {
@@ -14,3 +41,4 @@ function obtenerPersonaje(id) {
 obtenerPersonaje(1)
 obtenerPersonaje(2)
 obtenerPersonaje(3)
+*/
