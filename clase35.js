1a7b41a7675796d0d19f4e36584df3c1ae909581
deybit <deybitgutierrez@gmail.com>
2019-02-26 19:34:11 -0400

    Async-await

--- clase35.js
+++ clase35.js
@@ -1,5 +1,57 @@
 const API_URL = 'https://swapi.co/api/'
 const PEOPLE_URL = 'people/:id'
+
+const opts = { crossDomain: true }
+
+function obtenerPersonaje(id){
+  return new Promise((resolve, reject) => {
+    const URL = `${API_URL}${PEOPLE_URL.replace(':id',id)}`
+    $
+    .get(URL, opts, function(data){
+      resolve(data)
+    })
+    .fail(() => reject(id))
+
+  })
+
+
+
+
+  $
+  .get(URL, opts, callback)
+  .fail(function(){
+        console.log(`Error: No se pudo buscar al personaje ${id}`)
+    })
+
+
+}
+
+function onError(id){
+  console.log(`Error obteniendo en el personaje ${id}`)
+}
+
+async function obtenerPersonajes(){
+
+  var ids = [1, 2, 3, 4, 5, 6, 7]
+  var promesas = ids.map((id) => obtenerPersonaje(id))
+  try {
+    var personajes = await Promise.all(promesas)
+    console.table(personajes)
+  }catch (id){
+    onError(id)
+  }
+
+  Promise
+    .all(promesas)
+    .then(persona => console.log(persona))
+    .catch(onError)
+
+}
+
+obtenerPersonajes()
+
+/* const API_URL = 'https://swapi.co/api/'
+const PEOPLE_URL = 'people/:id'
 const opts = { crossDomain: true }
 
 function obtenerPersonaje(id) {
@@ -29,3 +81,4 @@ async function obtenerPersonajes() {
 }
 
 obtenerPersonajes()
+*/
